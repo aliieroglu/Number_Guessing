b72194320aa6be0df0eb2167847bf6afb203e6a9
aliieroglu <alibugraeroglu@gmail.com>
2023-11-07 00:01:33 +0300

    Add game over function and restart button function

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const resultParas = document.querySelector(".resultParas");
 const guesses = document.querySelector(".guesses");
 const lastResult = document.querySelector(".lastResult");
 const lowOrHi = document.querySelector(".lowOrHi");
+let inputRights = 8;
 
 let randomNumber = Math.floor(Math.random() * 100) + 1;
 
@@ -12,23 +13,21 @@ console.log(randomNumber);
 eventListenars();
 
 function eventListenars(){
-    form.addEventListener("submit",checkNumber);
+    form.addEventListener("click",checkNumber);
 }
 
 function checkNumber(e){
     const guessInput = inputNumber.value;
     console.log(guessInput);
 
-    let inputRights = 10;
-    
-    if(inputRights < 0){
-        lowOrHi.textContent = "hakkiniz bitti";
-        lowOrHi.style.backgroundColor = 'red';
-        resetGame();
+    if(inputRights === 8){
+        guesses.textContent = "Previous number : ";
     }
 
-    if(inputRights === 10){
-        guesses.textContent = "Previous number : ";
+    if(inputRights < 0){
+        lastResult.textContent = "Game over";
+        lowOrHi.textContent = '';
+        gameOver();
     }
 
 
@@ -37,27 +36,28 @@ function checkNumber(e){
     if(guessInput > randomNumber){
         lowOrHi.textContent = "Daha kucuk bir deger girin";
         lowOrHi.style.backgroundColor = 'red';
-
+        lowOrHi.style.padding = '10px';
     }
     else if(guessInput < randomNumber){
-        guesses.innerHTML = guessInput;
         lowOrHi.textContent = "Daha buyuk bir deger girin";
         lowOrHi.style.backgroundColor = 'red';
+        lowOrHi.style.padding = '10px';
     }
     else{
         lastResult.innerHTML = "Tebrikler DOGRU BILDINIZ";
         lastResult.style.backgroundColor = 'green';
         lastResult.padding = '15px';
-        resetGame();
+        lowOrHi.textContent = '';
+        gameOver();
     }
     inputRights--;
-    guessField.value = '';
-    guessField.focus();
+    inputNumber.value = '';
+    inputNumber.focus();
 
     e.preventDefault();
 }
 
-function resetGame() {
+function gameOver() {
     inputNumber.disabled = true;
     submitBtn.disabled = true;
     resetBtn = document.createElement("button");
@@ -65,10 +65,25 @@ function resetGame() {
     resetBtn.style.width = '10rem';
     resetBtn.style.height = '32px';
     document.body.appendChild(resetBtn);
-    resetBtn.addEventListener("submit",newGame);
+    resetBtn.addEventListener("click",newGame);
 }
 
 function newGame(){
+    inputRights = 8;
+
+    const resetParas = document.querySelectorAll(".resultParas p");
+    for(const resetPara of resetParas){
+        resetPara.textContent = '';
+    }
+
+    resetBtn.parentNode.removeChild(resetBtn);
+    inputNumber.disabled = false;
+    inputNumber.disabled = false;
+    inputNumber.value = '';
+    inputNumber.focus();
+    lastResult.style.backgroundColor = 'white';
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+
 
 
 }
